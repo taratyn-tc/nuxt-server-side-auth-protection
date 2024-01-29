99bb972fb1fa255ebb33e68f110e01aefdda1826
Tom Aratyn <taratyn@wavelo.com>
2024-01-29 15:40:29 -0500

    Nicer path filtering.

--- server/middleware/bad-auth.ts
+++ server/middleware/bad-auth.ts
@@ -1,8 +1,9 @@
-// file: ~/server/middleware/auth.ts
 import { getServerSession } from '#auth'
 
+const ALLOWED_PATHS= ['/api/auth/']
 export default eventHandler(async (event) => {
-  if  (event.path.startsWith('/api/auth')) {
+  if  (ALLOWED_PATHS.some(p => event.path.startsWith(p))) {
+    // this means we're allowed to proceed regardless of whether we're logged in or not.
     return
   }
   const session = await getServerSession(event)
